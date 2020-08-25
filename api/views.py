c91de35e601c335b2287be08c78f0cf778692e15
elidian <elidian89@gmail.com>
2020-08-24 22:35:27 -0300

    update index.html views.py

--- api/views.py
+++ api/views.py
@@ -1,10 +1,10 @@
 from api import app
-from flask import Flask, jsonify, request
+from flask import Flask, render_template
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return 'Home page.'
+    return render_template('index.html')
 
 @app.route('/contato')
 def contato():
